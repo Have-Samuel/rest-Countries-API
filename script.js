0179e5534247b6dd01a85e233f50cda0b3a31499
samuel <samiehave9@gmail.com>
2024-05-11 14:25:46 +0300

    Dark Mode function

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ drops.forEach((drop) => {
 });
 
 // Changing the whole app Dark Mode
+const darkMode = document.querySelector('.dark-mode');
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
+const sideBar = document.querySelector('.side-bar');
+const main = document.querySelector('main');
+const card = document.querySelectorAll('.card');
+const dropDown = document.querySelectorAll('.drop-down');
+
+darkMode.addEventListener('click', () => {
+  body.classList.toggle('dark');
+  header.classList.toggle('dark');
+  footer.classList.toggle('dark');
+  sideBar.classList.toggle('dark');
+  main.classList.toggle('dark');
+  card.forEach((card) => {
+    card.classList.toggle('dark');
+  });
+  dropDown.forEach((drop) => {
+    drop.classList.toggle('dark');
+  });
+});
+
