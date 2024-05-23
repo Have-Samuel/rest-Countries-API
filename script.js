4fdb893aa9a2914e201bd7f99125cabe5a20bbc1
samuel <samiehave9@gmail.com>
2024-05-23 12:58:50 +0300

    Writing the theme function

--- script.js
+++ script.js
@@ -2,4 +2,9 @@
 // 1. Get the element with the class of "toggle-theme"
 const theme = document.querySelector('.toggle-theme');
 
+// 2. Listen for a click event on the element
+theme.addEventListener('click', () => {
+  // 3. Toggle the class of the body
+  document.body.classList.toggle('dark-theme');
+});
 
